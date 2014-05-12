e01ea5321cde9a46b1d720ac36e716a2029651f9
juan.garibotti <juan.garibotti@baiwks0171.bai.gameloft.org>
2014-05-12 16:28:32 -0300

    - Modified the Collision Resolution Update method to react to boundary collisions. - Changed Collision Resolution to move collided objects away from each other so that no frame starts with objects in a collided state.

--- src/Components/CollisionResolution.cpp
+++ src/Components/CollisionResolution.cpp
@@ -17,17 +17,47 @@ CollisionResolutionComponent::CollisionResolutionComponent( MovementComponent & 
 
 void CollisionResolutionComponent::Update()
 {
+    BoundaryCheck const& boundaryCheck ( m_collisionDetection.GetBoundaryCheck() );
+    if ( boundaryCheck.m_entityID != 0 )
+    {
+        if ( boundaryCheck.m_entityID == 3 )
+        {
+            MovementData ball ( m_movement.GetData( boundaryCheck.m_entityID ) );
+            if ( boundaryCheck.m_side == BoundaryCheck::e_top || boundaryCheck.m_side == BoundaryCheck::e_bottom )
+            {
+                ball.m_position[1] -= ball.m_speed[1] / 60.0f;
+                ball.m_speed[1] *= -1.0f;
+                m_movement.SetData( ball.m_entityID, ball );
+            }
+        }
+        else
+        {
+            MovementData paddle ( m_movement.GetData( boundaryCheck.m_entityID ) );
+            if ( boundaryCheck.m_side == BoundaryCheck::e_top || boundaryCheck.m_side == BoundaryCheck::e_bottom )
+            {
+                paddle.m_position[1] -= paddle.m_speed[1] / 60.0f;
+                paddle.m_speed[1] = 0.0f;
+                m_movement.SetData( paddle.m_entityID, paddle );
+            }
+        }
+
+        m_collisionDetection.ClearCollisions();
+    }
+
     CollisionsList const& collisions ( m_collisionDetection.GetCollisions() );
     if ( collisions.size() != 0 )
     {
         MovementData entityA ( m_movement.GetData( collisions.front().m_entityIDA ) );
+        entityA.m_position[0] -= entityA.m_speed[0] / 60.0f;
         entityA.m_speed[0] *= -1.0f;
         m_movement.SetData( entityA.m_entityID, entityA );
 
         MovementData entityB ( m_movement.GetData( collisions.front().m_entityIDB ) );
+        entityB.m_position[0] -= entityB.m_speed[0] / 60.0f;
         entityB.m_speed[0] *= -1.0f;
         m_movement.SetData( entityB.m_entityID, entityB );
 
         m_collisionDetection.ClearCollisions();
     }
+
 }
