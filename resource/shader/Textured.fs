ddf6c73e950639f1eb5bdd829b9b1af3a1b2713b
Juan Pablo Garibotti Arias <jp.garibotti@gmail.com>
2014-06-10 20:26:50 -0300

    * Fixed textured shader to work with AMD card.

--- resource/shader/Textured.fs
+++ resource/shader/Textured.fs
@@ -2,10 +2,10 @@
 
 uniform sampler2D colorMap;
 
-in vec2 fragTexCoord;
+smooth in vec2 fragTexCoord;
 out vec4 fragColor;
 
 void main(void)
 {
-   fragColor = vec4( 1.0,1.0,1.0,1.0 ) * texture( colorMap, fragTexCoord ).x;
+   fragColor = vec4( 1.0, 1.0, 1.0, texture( colorMap, fragTexCoord ).x );
 }
